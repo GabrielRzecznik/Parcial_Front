a3444e7277bcb8808f3dbe83d4073788dc8a5cf7
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-09 19:41:27 -0300

    soyunmensaje

--- js/ajaxAuto.js
+++ js/ajaxAuto.js
@@ -10,7 +10,7 @@ function peticionRegistrarUsuario(formulario){
             if (xmlhttp.status == 200) {//Volvio Bien
                 var data = JSON.parse(xmlhttp.responseText);
                 console.log(data);
-                var data = Object.keys(data[0]);
+                var nombre_columnas = Object.keys(data[0]);
 
                 for (var i = 0; i < nombre_columnas.length; i++) {
                     colum.innerHTML = colum.innerHTML + '<th scope="col">' + nombre_columnas[i] + '</th>';
@@ -19,22 +19,21 @@ function peticionRegistrarUsuario(formulario){
                 //colum.innerHTML = colum.innerHTML + '<th scope="col">Editar</th>' + '<th scope="col">Borrar</th>' + '<th scope="col">Actualizar</th>';
 
                 for (var i = 0; i < data.length; i++) {
-
-                    con.innerHTML = con.innerHTML   + '<th class="table-white" scope="row"></th>' 
-                                                        + '<td class="table-white">' + data[i].patente + '</td>' 
-                                                        + '<td class="table-white">' + data[i].marca + '</td>'
-                                                        + '<td class="table-white">' + data[i].modelo + '</td>'
-                                                        + '<td class="table-white">' + data[i].version + '</td>'
-                                                        + '<td class="table-white">' + data[i].color + '</td>'
-                                                        + '<td class="table-white">' + data[i].estado + '</td>'
-                                                        + '<td class="table-white">' + data[i].cambio + '</td>'
-                                                        + '<td class="table-white">' + data[i].combustible + '</td>'
-                                                        + '<td class="table-white">' + data[i].valor + '</td>'
-                                                        + '<td class="table-white">' + data[i].kilometraje + '</td>'
-                                                        + '<td class="table-white">' + data[i].anio + '</td>'
-                                                        + '<td class="table-white">' + data[i].propietario + '</td>'
-                                                    + '</th>';
-                                                       
+                    con.innerHTML = con.innerHTML   
+                    + '<th class="table-white" scope="row"></th>' 
+                        + '<td class="table-white">' + data[i].patente + '</td>' 
+                        + '<td class="table-white">' + data[i].marca + '</td>'
+                        + '<td class="table-white">' + data[i].modelo + '</td>'
+                        + '<td class="table-white">' + data[i].version + '</td>'
+                        + '<td class="table-white">' + data[i].color + '</td>'
+                        + '<td class="table-white">' + data[i].estado + '</td>'
+                        + '<td class="table-white">' + data[i].cambio + '</td>'
+                        + '<td class="table-white">' + data[i].combustible + '</td>'
+                        + '<td class="table-white">' + data[i].valor + '</td>'
+                        + '<td class="table-white">' + data[i].kilometraje + '</td>'
+                        + '<td class="table-white">' + data[i].anio + '</td>'
+                        + '<td class="table-white">' + data[i].propietario + '</td>'
+                    + '</th>';                                       
                 }
             }else{
                 alert("Exploto todo!!");
