eb4e739d6dfcecaf68cd80e6fddcb483c894234c
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-09 18:11:48 -0300

    soyunmensaje

---
+++ js/ajaxAuto.js
@@ -0,0 +1,21 @@
+function peticionRegistrarUsuario(formulario){
+    var formData= new FormData(formulario); //Las keys corresponden al atributo name de cada elemento  
+    var formJSON=JSON.stringify(Object.fromEntries(formData));
+    console.log(formJSON);
+
+    xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+                var automovilesEncontrados = JSON.parse(xmlhttp.responseText);
+                alert(automovilesEncontrados);
+            }
+            else{
+                alert("Exploto todo!!");
+            }   
+        }
+    }
+    xmlhttp.open("GET",'https://parcial-edi-backend.herokuapp.com/Automovil/listaAutomovil',true);
+    xmlhttp.send();//No le mando
+}
