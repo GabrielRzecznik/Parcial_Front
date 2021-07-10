d4f8a869558ab671cc0976d206ef1932bccb6da2
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-10 17:45:37 -0300

    soyunmensaje

--- js/ajaxDatosCuenta.js
+++ js/ajaxDatosCuenta.js
@@ -9,8 +9,8 @@ function mostrarDatosUsuario(usuario){
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
                 var data=JSON.parse(xmlhttp.responseText);
-                console.log(data["correo"]);
-                mostrarCorreo.innerHTML = data[correo];
+                console.log(data);
+                mostrarCorreo.innerHTML = data;
             }else{
                 alert("No se pudieron traer los datos del usuario!");
             }   
