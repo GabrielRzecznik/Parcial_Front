b682785f29aae2b261d181f9522db2198546185f
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-10 18:10:25 -0300

    soyunmensaje

--- js/ajaxDatosCuenta.js
+++ js/ajaxDatosCuenta.js
@@ -13,6 +13,7 @@ function mostrarDatosUsuario(usuario){
             if (xmlhttp.status == 200) {//Volvio Bien
                 var data=JSON.parse(xmlhttp.responseText);
                 mostrarNombre.innerHTML = data[0]["nombre"];
+                mostrarApellido.innerHTML = data[0]["apellido"];
                 mostrarCorreo.innerHTML = data[0]["correo"];
                 mostrarProvincia.innerHTML = data[0]["provincia"];
             }else{
