8ce977f8fcb174e2e18f1f1fd92c5cdc99024806
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-10 16:33:47 -0300

    soyunmensaje

--- js/perfil.js
+++ js/perfil.js
@@ -1,6 +1,6 @@
 //Metodo Loud
 function perfil(){
     //Usuario
-    var correo = "rzecznik@gmail.com";
-    mostrarDatosUsuario(correo);
+    var usuario = "rzecznik@gmail.com";
+    mostrarDatosUsuario(usuario);
 }
