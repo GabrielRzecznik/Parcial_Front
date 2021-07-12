e161c16b6127e7b62faa3469608589b3ac14d4a0
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-12 13:05:06 -0300

    soyunmensaje

--- js/ajaxRegistro.js
+++ js/ajaxRegistro.js
@@ -7,13 +7,16 @@ function peticionRegistrarUsuario(formulario){
 
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
-            if (xmlhttp.status == 200) {//Volvio Bien
+            if (xmlhttp.status == 200) {
                 var respuestaResivida = xmlhttp.responseText;
                 alert(respuestaResivida);
+                window.location.href = "https://parcial-edi-front.herokuapp.com/index.html";
+            }if (xmlhttp.status == 500) {
+                alert("El correo ingresado ya corresponde a una cuenta existente, ingrese otro correo");
             }
             else{
-                alert("Exploto todo!!");
-            }   
+                alert("Fallo la conexión con el servidor!!");
+            }
         }
     }
     //xmlhttp.open("GET",'https://tp-final-pp-liv-ferz-backend.herokuapp.com/Usuarios/registro'+'/'+nombre+'/'+apellido,true);
