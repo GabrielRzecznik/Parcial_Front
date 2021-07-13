cc1b7a0d5d591b4763e1501a8331a81fb6755e6f
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-13 01:12:02 -0300

    soyunmensaje

--- js/ajaxBuscarUsuario.js
+++ js/ajaxBuscarUsuario.js
@@ -9,34 +9,34 @@ function buscarUsuario(formulario){
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
                 var data = JSON.parse(xmlhttp.responseText);
-                    for (let i = 0; i < data.length; i++) {
-                        if (correo.value === data[i]["correo"]) {
-                            if (contraseña.value === data[0]["contraseña"]) {
-                                window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
-                            }else{
-                                alert("La contraseña es incorrecta");
-                                //Logo de carga
-                                document.querySelector('#loguearse').classList.remove('invisible');
-                                document.querySelector('#cargando').classList.add('invisible');
-                                //Marcar Contraseña
-                                document.getElementById('iconoC').classList.remove('validado');
-                                document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
-                                document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
-                                contraseña.focus();
-                            }
+                for (let i = 0; i < data.length; i++) {
+                    if (correo.value === data[i]["correo"]) {
+                        if (contraseña.value === data[0]["contraseña"]) {
+                            window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
                         }else{
-                            alert("No se encontro el usuario!");
+                            alert("La contraseña es incorrecta");
                             //Logo de carga
-                            document.querySelector('#cargando').classList.add('invisible');//Logo de carga
-                            document.querySelector('#loguearse').classList.remove('invisible');//Esconde el texto del boton
-                            //Marcar Correo
-                            document.getElementById('iconoCorreo').classList.remove('validado');
-                            document.querySelector('#iconoCorreo').classList.add('bi-x-circle-fill');
-                            document.querySelector('#iconoCorreo').classList.remove('bi-check-circle-fill');
+                            document.querySelector('#loguearse').classList.remove('invisible');
+                            document.querySelector('#cargando').classList.add('invisible');
+                            //Marcar Contraseña
+                            document.getElementById('iconoC').classList.remove('validado');
+                            document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
+                            document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
+                            contraseña.focus();
                         }
-                    }   
-                }
+                    }else{
+                        alert("No se encontro el usuario!");
+                        //Logo de carga
+                        document.querySelector('#cargando').classList.add('invisible');//Logo de carga
+                        document.querySelector('#loguearse').classList.remove('invisible');//Esconde el texto del boton
+                        //Marcar Correo
+                        document.getElementById('iconoCorreo').classList.remove('validado');
+                        document.querySelector('#iconoCorreo').classList.add('bi-x-circle-fill');
+                        document.querySelector('#iconoCorreo').classList.remove('bi-check-circle-fill');
+                    }
+                }   
             }
+        }
     }
     xmlhttp.open("POST",'https://parcial-edi-backend.herokuapp.com/Usuarios/buscarUsuario',true);
     xmlhttp.send(formJSON);
