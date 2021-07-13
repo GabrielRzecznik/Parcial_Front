4a858fd214b56a0188c5047410027086389c1c51
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-13 01:08:44 -0300

    soyunmensaje

--- js/ajaxBuscarUsuario.js
+++ js/ajaxBuscarUsuario.js
@@ -9,20 +9,23 @@ function buscarUsuario(formulario){
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
                 var data = JSON.parse(xmlhttp.responseText);
-                if (correo.value === data[0]["correo"]) {
-                    if (contraseña.value === data[0]["contraseña"]) {
-                        window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
-                    }else{
-                        alert("La contraseña es incorrecta");
-                        //Logo de carga
-                        document.querySelector('#loguearse').classList.remove('invisible');
-                        document.querySelector('#cargando').classList.add('invisible');
-                        //Marcar Contraseña
-                        document.getElementById('iconoC').classList.remove('validado');
-                        document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
-                        document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
-                        contraseña.focus();
-                    }
+                    for (let i = 0; i < data.length; i++) {
+                        if (correo.value === data[i]["correo"]) {
+                            if (contraseña.value === data[0]["contraseña"]) {
+                                window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
+                            }else{
+                                alert("La contraseña es incorrecta");
+                                //Logo de carga
+                                document.querySelector('#loguearse').classList.remove('invisible');
+                                document.querySelector('#cargando').classList.add('invisible');
+                                //Marcar Contraseña
+                                document.getElementById('iconoC').classList.remove('validado');
+                                document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
+                                document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
+                                contraseña.focus();
+                            }
+                        }
+                    }   
                 }else{
                     alert("No se encontro el usuario!");
                     //Logo de carga
@@ -33,17 +36,7 @@ function buscarUsuario(formulario){
                     document.querySelector('#iconoCorreo').classList.add('bi-x-circle-fill');
                     document.querySelector('#iconoCorreo').classList.remove('bi-check-circle-fill');
                 }
-            }else{
-                alert("No se encontro el usuario!");
-                //Marcar Contraseña
-                document.getElementById('iconoC').classList.remove('validado');
-                document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
-                document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
-                //Marcar Correo
-                document.getElementById('iconoCorreo').classList.remove('validado');
-                document.querySelector('#iconoCorreo').classList.add('bi-x-circle-fill');
-                document.querySelector('#iconoCorreo').classList.remove('bi-check-circle-fill');
-            }   
+            }
         }
     }
     xmlhttp.open("POST",'https://parcial-edi-backend.herokuapp.com/Usuarios/buscarUsuario',true);
