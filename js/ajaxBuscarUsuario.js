c731e7fdadf59f0d01a535dc81e8c681c5a79545
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-12 23:59:52 -0300

    soyunmensaje

--- js/ajaxBuscarUsuario.js
+++ js/ajaxBuscarUsuario.js
@@ -9,8 +9,12 @@ function buscarUsuario(formulario){
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
                 var data = JSON.parse(xmlhttp.responseText);
-                if (correo.value === data[0]["correo"]) {//TESTEAR
-                    window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
+                if (correo.value === data[0]["correo"]) {
+                    if (contraseña.value === data[0]["contraseña"]) {
+                        window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
+                    }else{
+                        alert("La contraseña es incorrecta");
+                    }
                 }else{
                     alert("No se encontro el usuario!");
                 }
