c9208bc086cd83fc70a5427e7ce3a4649db2dade
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-13 20:56:03 -0300

    soyunmensaje

---
+++ js/ajaxEliminarUsuario.js
@@ -0,0 +1,18 @@
+function eliminarUsuario(){
+    var formJSON=JSON.stringify({"correo":usuario, "contraseña":contraseña});
+    console.log(formJSON);
+
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+                alert("Usuario eliminado!");
+                window.location.href = "https://parcial-edi-front.herokuapp.com/index.html";
+            }else{
+                alert("No se pudo eliminar el usuario!");
+            }   
+        }
+    }
+    xmlhttp.open("DELETE",'https://parcial-edi-backend.herokuapp.com/Usuarios/eliminarUsuario',true);
+    xmlhttp.send(formJSON);
+}
--- js/perfil.js
+++ js/perfil.js
@@ -19,26 +19,6 @@ function datosPerfil(){
 var botonEliminar = document.getElementById("eliminar");
 botonEliminar.addEventListener('click', eliminarUsuario);
 
-//Pegar tablita
-function eliminarUsuario(){
-    var formJSON=JSON.stringify({"correo":usuario, "contraseña":contraseña});
-    console.log(formJSON);
-
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
-            if (xmlhttp.status == 200) {//Volvio Bien
-                alert("Usuario eliminado!");
-                window.location.href = "https://parcial-edi-front.herokuapp.com/index.html";
-            }else{
-                alert("No se pudo eliminar el usuario!");
-            }   
-        }
-    }
-    xmlhttp.open("DELETE",'https://parcial-edi-backend.herokuapp.com/Usuarios/eliminarUsuario',true);
-    xmlhttp.send(formJSON);
-}
-
 //Editar Usuario
 var botonEditar = document.getElementById("editar");
 botonEditar.addEventListener('click', editarUsuario);
