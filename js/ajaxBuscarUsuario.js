2e2008c39b6579e8f3187f9fb3921b2e055782ed
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-13 11:12:16 -0300

    soyunmensaje

--- js/ajaxBuscarUsuario.js
+++ js/ajaxBuscarUsuario.js
@@ -10,45 +10,6 @@ function buscarUsuario(formulario){
             if (xmlhttp.status == 200) {//Volvio Bien
                 //var data = JSON.parse(xmlhttp.responseText);
                 window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
-                if (contraseña.value === data[i]["contraseña"]) {
-                    window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
-                }else{
-                    alert("La contraseña es incorrecta");
-                    //Logo de carga
-                    document.querySelector('#loguearse').classList.remove('invisible');
-                    document.querySelector('#cargando').classList.add('invisible');
-                    //Marcar Contraseña
-                    document.getElementById('iconoC').classList.remove('validado');
-                    document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
-                    document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
-                    contraseña.focus();
-                }
-                /*for (let i = 0; i < data.length; i++) {
-                    if (correo.value === data[i]["correo"]) {
-                        if (contraseña.value === data[i]["contraseña"]) {
-                            window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
-                        }else{
-                            alert("La contraseña es incorrecta");
-                            //Logo de carga
-                            document.querySelector('#loguearse').classList.remove('invisible');
-                            document.querySelector('#cargando').classList.add('invisible');
-                            //Marcar Contraseña
-                            document.getElementById('iconoC').classList.remove('validado');
-                            document.querySelector('#iconoC').classList.add('bi-x-circle-fill');
-                            document.querySelector('#iconoC').classList.remove('bi-check-circle-fill');
-                            contraseña.focus();
-                        }
-                    }else{
-                        alert("No se encontro el usuario!");
-                        //Logo de carga
-                        document.querySelector('#cargando').classList.add('invisible');//Logo de carga
-                        document.querySelector('#loguearse').classList.remove('invisible');//Esconde el texto del boton
-                        //Marcar Correo
-                        document.getElementById('iconoCorreo').classList.remove('validado');
-                        document.querySelector('#iconoCorreo').classList.add('bi-x-circle-fill');
-                        document.querySelector('#iconoCorreo').classList.remove('bi-check-circle-fill');
-                    }
-                }*/  
             }if (xmlhttp.status == 401) {
                 alert("No se encontro el usuario!");
             }
