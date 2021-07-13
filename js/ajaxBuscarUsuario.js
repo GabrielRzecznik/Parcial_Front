c076c40761aaf5860f66fc0e7963b80534e4b895
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-12 22:25:56 -0300

    soyunmensaje

---
+++ js/ajaxBuscarUsuario.js
@@ -0,0 +1,20 @@
+//Pegar tablita
+function mostrarDatosUsuario(formulario){
+    var formData= new FormData(formulario);
+    var formJSON=JSON.stringify(Object.fromEntries(formData));
+    console.log(formJSON);
+
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
+            if (xmlhttp.status == 200) {//Volvio Bien
+                window.location.href = "https://parcial-edi-front.herokuapp.com/inicio.html";
+            }else{
+                alert("No se encontro el usuario!");
+            }   
+        }
+    }
+    xmlhttp.open("POST",'https://parcial-edi-backend.herokuapp.com/Usuarios/buscarUsuario',true);
+    xmlhttp.send(formJSON);
+}
+
