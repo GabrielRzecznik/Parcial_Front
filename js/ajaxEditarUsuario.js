7ec8be4cc968cad19f60e288e7443017fcd111a9
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-14 11:32:31 -0300

    soyunmensaje

--- js/ajaxEditarUsuario.js
+++ js/ajaxEditarUsuario.js
@@ -6,13 +6,14 @@ function editarUsuario(){
     xmlhttp.onreadystatechange = function () {//Cuando hay cambio de estado disparo la function
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {//Volvio respuesta
             if (xmlhttp.status == 200) {//Volvio Bien
-                alert("Usuario actualizado!");
+                var respuestaResivida = xmlhttp.responseText;
+                alert(respuestaResivida);
                 window.location.href = "https://parcial-edi-front.herokuapp.com/perfil.html";
             }else{
                 alert("No se pudo actualizar el usuario!");
             }   
         }
     }
-    xmlhttp.open("POST",'https://parcial-edi-backend.herokuapp.com/Usuarios/actualizarUsuario',true);
+    xmlhttp.open("PUT",'https://parcial-edi-backend.herokuapp.com/Usuarios/actualizarUsuario',true);
     xmlhttp.send(formJSON);
 }
