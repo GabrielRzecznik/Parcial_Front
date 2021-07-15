e847a7257c41540e8cfe0124c6c4160a013cab3b
Gabriel Rzecznik <rzecznike@gmail.com>
2021-07-15 01:47:01 -0300

    soyunmensaje

--- js/perfil.js
+++ js/perfil.js
@@ -316,3 +316,17 @@ function enviarActualizacionUsuario() {
        }
     }); 
 }
+
+//Menu Pestañas
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+	target.addEventListener('click', () => {
+		content.forEach(c => {
+			c.classList.remove('active')
+		})
+		const t = document.querySelector(target.dataset.target)
+		t.classList.add('active')
+	})
+})
